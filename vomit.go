923d264e105d226f4b134ad3dd990035223f1d5d
Walter Jones <wjones17@dccc.edu>
2015-02-27 17:45:44 -0500

    go fmt

--- vomit.go
+++ vomit.go
@@ -1,80 +1,80 @@
 package main
 
 import (
-    "flag"
-    "fmt"
-    "github.com/russross/blackfriday"
-    "io/ioutil"
-    "time"
-    "path/filepath"
-    "strings"
-    "os"
+	"flag"
+	"fmt"
+	"github.com/russross/blackfriday"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+	"time"
 )
 
 func DatePath() string {
-    year := time.Now().Year()
-    month := int(time.Now().Month())
-    day := time.Now().Day()
+	year := time.Now().Year()
+	month := int(time.Now().Month())
+	day := time.Now().Day()
 
-    return fmt.Sprintf("%d/%d/%d/", year, month, day)
+	return fmt.Sprintf("%d/%d/%d/", year, month, day)
 }
 
 func Add(filename string) {
 
-    // Check if filename exists
-    if _, err := os.Stat(filename); os.IsNotExist(err) {
-        fmt.Println(err)
-        return
-    }
-    
-    // Set title
-    post := strings.TrimSuffix(filename, filepath.Ext(filename))
+	// Check if filename exists
+	if _, err := os.Stat(filename); os.IsNotExist(err) {
+		fmt.Println(err)
+		return
+	}
 
-    // Set Path
-    datePath := DatePath()
-    fullpath := datePath + post
+	// Set title
+	post := strings.TrimSuffix(filename, filepath.Ext(filename))
 
-    // Create path
-    err := os.MkdirAll(fullpath, 0755)    
-    if err != nil {
-        fmt.Println(err)
-    }
+	// Set Path
+	datePath := DatePath()
+	fullpath := datePath + post
 
-    // Convert
-    markup, err := DownToUp(filename)
-    if err != nil {
-        fmt.Println(err)
-    }
+	// Create path
+	err := os.MkdirAll(fullpath, 0755)
+	if err != nil {
+		fmt.Println(err)
+	}
 
-    // Write markup
-    newpost := fullpath + "/index.html"
-    err = ioutil.WriteFile(newpost, markup, 0755) 
-    if err != nil {
-        fmt.Println(err)
-    }
+	// Convert
+	markup, err := DownToUp(filename)
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	// Write markup
+	newpost := fullpath + "/index.html"
+	err = ioutil.WriteFile(newpost, markup, 0755)
+	if err != nil {
+		fmt.Println(err)
+	}
 
 }
 
 func DownToUp(fn string) ([]byte, error) {
-    file, err := ioutil.ReadFile(fn) 
-    if err != nil {
-        return file, err
-    }
+	file, err := ioutil.ReadFile(fn)
+	if err != nil {
+		return file, err
+	}
 
-    markup := blackfriday.MarkdownBasic(file)
+	markup := blackfriday.MarkdownBasic(file)
 
-    return markup, err
+	return markup, err
 }
 
 func main() {
 
-    flag.Parse()
-   
-    if flag.NArg() < 1 {
-        fmt.Fprintf(os.Stderr, "No command given.\n")
-    } 
+	flag.Parse()
 
-    if flag.Arg(0) == "add" {
-        Add(flag.Arg(1))
-    }
+	if flag.NArg() < 1 {
+		fmt.Fprintf(os.Stderr, "No command given.\n")
+	}
+
+	if flag.Arg(0) == "add" {
+		Add(flag.Arg(1))
+	}
 }
