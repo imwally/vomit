a03c7f63a67c67cde4785365cef378e8a8d0e429
Wally <wally@imwally.net>
2015-03-04 02:28:17 -0500

    fmt

--- vomit.go
+++ vomit.go
@@ -1,87 +1,86 @@
 package main
 
 import (
-    "fmt"
-    "log"
+	"fmt"
+	"log"
 	//"github.com/russross/blackfriday"
 	"io/ioutil"
 	"os"
 	"path/filepath"
-    "strings"
+	"strings"
 )
 
 const (
-    postdir = "posts"
-    sitedir = "site"
+	postdir = "posts"
+	sitedir = "site"
 )
 
 type Post struct {
-    Title   string
-    Date    string
-    Content []byte
+	Title   string
+	Date    string
+	Content []byte
 }
 
 func GeneratePostPage(post Post) {
-    // Write post html page
+	// Write post html page
 }
 
 func GenerateIndexPage() {
-    // Write index html page
+	// Write index html page
 }
 
 func GetPost(p string) Post {
-    var post Post
-    
-    basename := strings.TrimSuffix(p, filepath.Ext(p))
-    post.Date = basename[:10]
-    post.Title = basename[11:]
-    content, err := ioutil.ReadFile(p)
-    CheckErr(err)
+	var post Post
 
-    post.Content = content
+	basename := strings.TrimSuffix(p, filepath.Ext(p))
+	post.Date = basename[:10]
+	post.Title = basename[11:]
+	content, err := ioutil.ReadFile(p)
+	CheckErr(err)
 
-    return post
+	post.Content = content
+
+	return post
 }
 
 func FindPosts(p string) []Post {
-    var posts []Post
+	var posts []Post
 
-    find := func(p string, f os.FileInfo, err error) error {
-        if !f.IsDir() {
-            posts = append(posts, GetPost(p))
-        }
-        return nil
-    }
+	find := func(p string, f os.FileInfo, err error) error {
+		if !f.IsDir() {
+			posts = append(posts, GetPost(p))
+		}
+		return nil
+	}
 
-    err := filepath.Walk(postdir, find)
-    CheckErr(err)
+	err := filepath.Walk(postdir, find)
+	CheckErr(err)
 
-    return posts
+	return posts
 }
 
 func CheckErr(err error) {
-    if err != nil {
-        log.Println(err)
-    }
+	if err != nil {
+		log.Println(err)
+	}
 }
 
 func main() {
 
-    // Check for posts directory
-    _, err := os.Stat(postdir) 
-    if err != nil {
-        fmt.Println("no posts directory found.")
-        return
-    }
+	// Check for posts directory
+	_, err := os.Stat(postdir)
+	if err != nil {
+		fmt.Println("no posts directory found.")
+		return
+	}
 
-    // Gather posts
-    posts := FindPosts(postdir)
+	// Gather posts
+	posts := FindPosts(postdir)
 
-    // Generate post pages
-    for _, post := range posts {
-        GeneratePostPage(post)
-    }
+	// Generate post pages
+	for _, post := range posts {
+		GeneratePostPage(post)
+	}
 
-    // Generate index page
+	// Generate index page
 }
-
